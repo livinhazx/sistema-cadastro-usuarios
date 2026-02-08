10698b5a7e912cbf767fa7b085b182ad3accfc12
livia <annalivdeveloper@gmail.com>
2026-02-08 11:21:39 -0300

    add: new validation for the birthdate and some fixes to the indentation, adding loop to the adduser function

--- system.py
+++ system.py
@@ -1,30 +1,52 @@
+from datetime import datetime
 
-def validEmail():
+def validEmail(email):
     if "@" not in email:
         return False
     pieces = email.split("@")
-    if len(pieces) !=2:
+    if len(pieces) != 2:
         return False
     user, domain = pieces
-    if user =="" or domain =="":
+    if user == "" or domain == "":
         return False
     if "." not in domain:
         return False
     if domain.startswith(".") or domain.endswith("."):
         return False
     return True
-   
 
 def addUser():
-    name = input("What's your name?")
-    if name.strip() == "":
-        print("Empty field! Type something.")
-    elif name.isdigit():
-        print("Only numbers? Type text.")
-    else: 
-        print("Name added.")
-    email = input("What's your best e-mail?")
-    validEmail()
-    
-    birthdate = input("What's your birthdate? Type DD MM AA format")
+    while True:
+        name = input("What's your name? ")
+        if name.strip() == "":
+            print("Empty field! Type something.")
+            continue
+        elif name.isdigit():
+            print("Only numbers? Type text.")
+            continue
+        else:
+            print("Name added.")
+        
+        email = input("What's your best e-mail? ")
+        if not validEmail(email):
+            print("Invalid email!")
+            continue
+        else:
+            print("Email valid!")
 
+        while True:
+            birthdate = input("What's your birthdate? Type DD/MM/YYYY format: ")
+            try:
+                date = datetime.strptime(birthdate, "%d/%m/%Y")
+                if date > datetime.now():
+                    print("Invalid date! You can't be born in the future.")
+                else:
+                    print("Valid date:", birthdate)
+                    break
+            except ValueError:
+                print("Invalid format! Use the DD/MM/YYYY format.")
+
+        print("User registered!")
+        
+
+addUser()
