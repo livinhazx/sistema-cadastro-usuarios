68d1b4d6d2a121b21fab89536158a86029dd12c0
livia <annalivdeveloper@gmail.com>
2026-02-08 11:07:07 -0300

    add: email validation

--- system.py
+++ system.py
@@ -1,4 +1,20 @@
 
+def validEmail():
+    if "@" not in email:
+        return False
+    pieces = email.split("@")
+    if len(pieces) !=2:
+        return False
+    user, domain = pieces
+    if user =="" or domain =="":
+        return False
+    if "." not in domain:
+        return False
+    if domain.startswith(".") or domain.endswith("."):
+        return False
+    return True
+   
+
 def addUser():
     name = input("What's your name?")
     if name.strip() == "":
@@ -8,6 +24,7 @@ def addUser():
     else: 
         print("Name added.")
     email = input("What's your best e-mail?")
-
+    validEmail()
+    
     birthdate = input("What's your birthdate? Type DD MM AA format")
 
